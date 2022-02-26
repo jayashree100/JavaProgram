2672297f9e5782a319a75ac6a7c9ca17e38e60d9
JAISHRI <sjayashree529@gmail.com>
2022-02-26 17:24:44 +0530

    adding the value

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,11 @@ class  Hello
 {
       public static void main(String[] args)
        {
-              
+              int a=10;
+              int b=20; 
              System.out.println("Hello ");
              System.out.println("second message");
+             System.out.println(a+b);
+             
        }
 }
