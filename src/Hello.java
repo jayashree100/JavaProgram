c17d58e75306a4419a02d8a46d8376b8468deb0a
JAISHRI <sjayashree529@gmail.com>
2022-02-26 16:33:14 +0530

    slave-4

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,8 @@ class  Hello
 {
       public static void main(String[] args)
        {
-              System.out.println("Hello ");
+              
+             System.out.println("Hello ");
+             System.out.println("second message");
        }
 }
