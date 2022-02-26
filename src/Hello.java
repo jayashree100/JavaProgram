3e5b80a5234c628f4e3b1081978dad16b784132c
JAISHRI <sjayashree529@gmail.com>
2022-02-26 17:33:33 +0530

    added

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,11 @@ class  Hello
               int b=20; 
              System.out.println("Hello ");
              System.out.println("second message");
+<<<<<<< HEAD
              System.out.println(a+b);
              
+=======
+            System.out.println("Third message");
+>>>>>>> 7c1a592d31b94fe6c54d2ba60ed6fcc5e0df986a
        }
 }
